d9a07f45590c280900447c7ac586965395f650af
TKerBell <47965866+TKerBell@users.noreply.github.com>
2020-02-07 16:09:11 +0800

    2020-02-07

---
+++ 20200207am/README.md
@@ -0,0 +1,2 @@
+# YP-basement-helper-front
+ 元培学院地下室预约小程序前端
---
+++ 20200207am/basement-helper/app.js
@@ -0,0 +1,32 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    //this.globalData.res = wx.getSystemInfoSync()//获取屏幕尺寸
+  },
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData: {
+    userInfo: null,
+    //res:null
+  }
+})
