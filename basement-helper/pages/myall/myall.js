cfa5fa0b8ad5d7f2c33399a205d416452c74f3ee
yabukota <wangxiaoyu2001@pku.edu.cn>
2020-03-06 22:14:55 +0800

    取消预约后的页面更新 机制改得更准确了一点

--- basement-helper/pages/myall/myall.js
+++ basement-helper/pages/myall/myall.js
@@ -6,10 +6,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    me:{}
+    me: {}
   },
 
-  cancelAppointment:function(options){
+  cancelAppointment: function(options) {
     var that = this
     var d = options.currentTarget.dataset
     wx.request({
@@ -22,31 +22,39 @@ Page({
         'content-type': 'application/json'
       },
       dataType: 'json',
-      success: function (res) {
-        console.log(that.data)
+      success: function(res) {
         wx.showToast({
           title: '已取消预约',
           icon: 'success',
           duration: 3000
         })
-
-        var tempme=that.data.me
-        console.log('tempme',tempme)
-        tempme.appointmentlist.data[d.delindex].Astatus='Canceled'
-        that.setData({
-          me: tempme//似乎不太好？
+        wx.request({
+          url: 'http://39.107.70.176:9000/appointment/get-student',
+          method: 'POST',
+          data: {
+            'Sid': app.globalData.me.id
+          },
+          header: {
+            'content-type': 'application/json'
+          },
+          success: function (res) {
+            app.globalData.me.appointmentlist = res.data
+            //用全局变量更新本地变量
+            that.setData({
+              me: app.globalData.me
+            })
+          }
         })
-
       }
     })
 
-    
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       me: app.globalData.me
     })
@@ -56,51 +64,69 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    this.setData({
-      me: app.globalData.me
+  onShow: function() {
+    //index页面每次显示的时候都要自动获取新的预约信息 并同步更新全局变量和本地变量    
+    //根据学号获取预约信息
+    var that = this
+    console.log('request id:', app.globalData.me.id)
+    wx.request({
+      url: 'http://39.107.70.176:9000/appointment/get-student',
+      method: 'POST',
+      data: {
+        'Sid': app.globalData.me.id
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function(res) {
+        app.globalData.me.appointmentlist = res.data
+        //用全局变量更新本地变量
+        that.setData({
+          me: app.globalData.me
+        })
+      }
     })
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
