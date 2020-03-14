eb5ecc7f41ffcfd5a75a8578ac35c5cbb4e98fb6
chunfenri <yangliming2018@pku.edu.cn>
2020-03-14 18:48:15 +0800

    index最近一条内容

--- 20200208pm/basement-helper/pages/index/index.js
+++ 20200208pm/basement-helper/pages/index/index.js
@@ -8,7 +8,10 @@ Page({
     me:{},//姓名学号预约信息
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    earliest:{}
+    earliest:{},
+    nextAppointTime:"",
+    nextAppointRoom:""
+
   },
 
 //跳转到对应自习室的sheet页面
@@ -44,8 +47,48 @@ Page({
 
   onShow: function () {
     this.setData({
-      me: app.globalData.me
+      me: app.globalData.me,
     })
+    var appointList = app.globalData.me.appointmentlist.data
+    var nextApt
+    var nextAppointStr = ""
+    if(appointList.length){
+      nextApt = appointList[0]
+      for(var i = 1; i < appointList.length; i++ ){
+          if(nextApt.Astart > appointList[i].Astart)
+            nextApt = appointList[i];
+      }
+      var ShowStr = ""
+      if(nextApt.Astart[5]=='1'){
+        ShowStr+='1'
+      }
+      ShowStr+=nextApt.Astart[6]
+      ShowStr+="月"
+      if (nextApt.Astart[8] != '0') {
+        ShowStr += nextApt.Astart[8]
+      }
+      ShowStr+=nextApt.Astart[9]
+      ShowStr+="日 "
+      ShowStr+=nextApt.Astart.slice(11,16)
+      ShowStr+="-"
+      ShowStr+=nextApt.Afinish.slice(11,16)
+      ShowStr+='\n'
+      
+
+      this.setData({
+        nextAppointTime: ShowStr,
+        nextAppointRoom: nextApt.Rtitle
+      })
+
+      console.log('me',app.globalData.me)
+      console.log('appointlist',appointList)
+      console.log('nextAppoint',nextApt)
+      console.log('debug', nextAppointStr)
+
+      
+
+    }
+
   },
 
   onReady: function () {
